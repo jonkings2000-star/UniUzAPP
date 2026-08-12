7d756a034f4b8fd3a74ffe65446d78cbda2f8dc6
jonkings2000-star <jonkings2000@gmail.com>
2026-08-12 09:31:03 +0300

    Add files via upload

--- server.py
+++ server.py
@@ -1,9 +1,6 @@
 import os
-import threading
 from web_api import app
-from reminders import start_reminder_worker
 
 if __name__ == "__main__":
-    start_reminder_worker()
     port = int(os.environ.get("PORT", "8080"))
     app.run(host="0.0.0.0", port=port)
