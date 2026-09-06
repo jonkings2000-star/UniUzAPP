8c88f0a79cdcdf8e99e61368fe116d339c29b77a
jonkings2000-star <jonkings2000@gmail.com>
2026-09-06 22:46:40 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -199,6 +199,11 @@ layout(`
  <button class="quick-v2" onclick="showProfile()">
   <span>👤</span>${tr("profile")}
  </button>
+
+ <button class="quick-v2" onclick="showReminders()">
+  <span>🔔</span>${tr("reminders")}
+ </button>
+
  ${profile?.is_admin ? `
  <button class="quick-v2 admin-v2" onclick="showAdmin()">
   <span>🔐</span>${tr("admin")}
